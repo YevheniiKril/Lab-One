bc880cec51fb2162d7c107c2a6de09b00c08784b
Yevhenii <mikjk5717@gmail.com>
2020-05-28 19:43:13 -0400

    Final file

---
+++ lab-01.js
@@ -0,0 +1,75 @@
+/*
+  Full Name: Yevhenii Kril
+  Student ID: 200409606
+
+  Instructions:
+    Follow the steps below. If you have any questions
+    please feel free to use Slack to communicate. You
+    are allowed to discuss the lab with other students
+    but you ARE NOT permitted to copy/paste or share
+    code.
+  
+  Rubric:
+    Steps: 11.5 points
+    Code Format: 10 points
+    Github Link: 10 points
+
+  NOTE: The code format and Github link marks are
+    only awarded if the steps have been completed.
+    (Code formatting is the visual structure of
+      you code. I expect that you have used proper
+      tabbing of nested structures so code readability
+      is adequate.)
+*/
+
+
+/*
+  Step 1 (2.5 points):
+    Create an array that contains 4 people objects.
+    Each object must have at least 2 properties.
+*/
+   var person = [
+  {
+    name: "Jack",
+    age: "22"
+  },
+  {
+    name:"Bob",
+    age:"14"
+
+  },
+  {
+    name: "Sam",
+    age: "25"
+  },
+  {
+    name: "Michael",
+    age: "34"
+
+  }
+  ]
+
+   
+
+/*
+  Step 2 (6 points):
+    Create an arrow function that accepts
+    a destructured object as an argument. In the
+    body of your function, console.log a string
+    template utilizing the properties you
+    destructured.
+*/
+
+const func1 = (counter) => {
+console.log( person[counter].name);
+}
+
+/*
+  Step 3 (3 points):
+    Loop over your array using a for/of loop.
+    Call your arrow function passing the array item
+    as an argument.
+*/
+for (i = 0; i < 2; i++) {
+  func1(i);
+}
